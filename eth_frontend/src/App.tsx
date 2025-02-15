24df9c7e5c1f9acaa6365cf414fbf910e22449fc
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-15 07:05:46 +0530

    added allowances

--- eth_frontend/src/App.tsx
+++ eth_frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useConnectors, WagmiProvider } from 'wagmi'
+import { useAccount, useConnect, useConnectors, useDisconnect, useReadContract, WagmiProvider } from 'wagmi'
 import './App.css'
 import { config } from './config'
+import { AllowUSDT } from './AllowUSDT'
 
 const client = new QueryClient()
 
@@ -11,17 +12,58 @@ function App() {
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
         <ConnectWallet />
+        <TotalSuply />
+        <AllowUSDT/> 
       </QueryClientProvider>
     </WagmiProvider>
   )
 }
 
-function ConnectWallet() {
-  const connectors = useConnectors()
+function TotalSuply() {
+  const { data, isLoading, error } = useReadContract({
+    address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+    abi: [
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ],
+    functionName: 'totalSupply',
+  })
 
   return <div>
+    Total supply is {data}
+  </div>
+}
+
+function ConnectWallet() {
+  const { address } = useAccount();
+  const connectors = useConnectors()
+  const { disconnect } = useDisconnect()
+
+  const { connect } = useConnect();
+
+  if (address) {
+    return <div>
+      You are connected {address}
+      <button onClick={() => { disconnect() }}>Disconnect</button>
+    </div>
+  }
+  return <div>
     {
-      connectors.map(c => <button>Connect via {c.name}</button>)
+      connectors.map(connector => <button onClick={() => { connect({ connector: connector }) }}>
+        Connect via {connector.name}
+      </button>)
     }
   </div>
 }
